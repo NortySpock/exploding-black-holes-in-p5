03bcafa55917579f70bde1854ffe06b6f2b55a92
David Norton <david.k.norton@gmail.com>
2018-02-06 21:43:50 -0600

    Got it running in p5.js. Still an odd issue with being able to click outside the black holes.

--- sketch.js
+++ sketch.js
@@ -1,15 +1,16 @@
 "use strict";
- 
+
 var canvasWidth = 700;
 var canvasHeight = 700;
 var maxDimension = Math.max(canvasWidth, canvasHeight);
 var blackHoleFill = 0;
 var whiteSpaceFill = 255;
 
-var array_width = 10;
+var array_width = 3;
 var x_array = new Array(array_width);
 var y_array = new Array(array_width);
 var radius_array = new Array(array_width);
+var color_array = new Array(array_width);
 var BlackHolesExploded = 0;
 
 var numberOfBlackHoles = 0;
@@ -17,6 +18,7 @@ var availibleSlot = 0;
 
 var isDead = false;
 var mousePressed = true;
+var expansionRadius = 10;
 
 
 function setup() {
@@ -24,7 +26,7 @@ function setup() {
 }
 
 function draw() {
-background(whiteSpaceFill);
+        background(whiteSpaceFill);
         fill(blackHoleFill);
         numberOfBlackHoles = 0;
 
@@ -36,25 +38,19 @@ background(whiteSpaceFill);
             if (isDead) {
                 radius = radius_array[i];
             } else {
-                radius = radius_array[i] = radius_array[i] + 10;
+                radius = radius_array[i] = radius_array[i] + expansionRadius;
             }
 
             if (xpos > 0 && ypos > 0 && radius > 0) {
                 numberOfBlackHoles++;
-                // if we hit a circle
-                if (!isDead && mousePressed == true && mouseButton == LEFT
-                        && dist(mouseX, mouseY, xpos, ypos) <= radius) {
-                    fill(whiteSpaceFill);
+                fill(color_array[i]);
+                ellipse(xpos, ypos, radius, radius);
 
+                //if this is a whitespace circle, we have already rendered it and it's time to clear it
+                if (color_array[i]==whiteSpaceFill) {
                     // clear this circle
                     x_array[i] = y_array[i] = radius_array[i] = 0;
-
-                    BlackHolesExploded++;
-                } else {
-                    fill(blackHoleFill);
                 }
-
-                ellipse(xpos, ypos, radius, radius);
 
                 // check to see if we lost
                 if (radius > maxDimension) {
@@ -71,14 +67,14 @@ background(whiteSpaceFill);
             x_array[availibleSlot] = random(canvasWidth);
             y_array[availibleSlot] = random(canvasHeight);
             radius_array[availibleSlot] = random(50) + 1;
+            color_array[availibleSlot] = blackHoleFill;
         }
 
         if (isDead) {
             textAlign(CENTER, CENTER);
             fill(128);
             text("Game Over.", canvasWidth / 2, canvasHeight / 4);
-            text("Zapped:" + BlackHolesExploded, canvasWidth / 2, canvasHeight * 3
-                    / 4);
+            text("Zapped:" + BlackHolesExploded, canvasWidth / 2, canvasHeight * (3 / 4));
         }
 
     }
@@ -88,21 +84,31 @@ function reset(){
 
     frameRate(20);
     textSize(60);
-    
+
     for (var i = 0; i < array_width; i++) {
         x_array[i] = 0;
         y_array[i] = 0;
         radius_array[i] = 0;
     }
-    
+
     numberOfBlackHoles = 0;
     isDead = false;
     availibleSlot = 0;
 
 }
 
-// function mousePressed(){
-  
-// }
+function mouseClicked(){
+  if(!isDead)
+  {
+    for(var i=0;i<array_width;i++)
+    {
+      if(dist(mouseX, mouseY, x_array[i], y_array[i]) <= radius_array[i])
+      {
+        color_array[i] = whiteSpaceFill;
+        BlackHolesExploded++;
+      }
+    }
+  }
+  return false;
+}
 
- 
